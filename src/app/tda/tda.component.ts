5328a4f4568defe5a88db1b5032fea1d03db5148
raresmigea <raresini@yahoo.com>
2020-10-14 23:59:18 +0300

    fixed comments

--- src/app/tda/tda.component.ts
+++ src/app/tda/tda.component.ts
@@ -12,18 +12,18 @@ export class TdaComponent implements OnInit {
   currentDate = '';
   defaultDate = undefined;
 
-  /** The values used for the intervals of Time Dependent Attribute */
+  /* The values used for the intervals of Time Dependent Attribute */
   firstDate = new Date('2018-12-12');
   secondDate = new Date('2019-12-12');
   thirdDate = new Date('2020-12-12');
 
-  /**Computing the value of today's date */
+  /* Computing the value of today's date */
   day = String(this.todayFull.getDate()).padStart(2, '0');
   month = String(this.todayFull.getMonth() + 1).padStart(2, '0');
   year = this.todayFull.getFullYear();
   today = `${this.year}-${this.month}-${this.day}`;
 
-  /** checkDate() computes if the date has TDA active or not */
+  /* checkDate() computes if the date has TDA active or not */
   checkDate(dateToCheck) {
     if (!dateToCheck) {
       dateToCheck = this.todayFull;
